fe59fddac60662642e1f32bcbcf15cbdb0439124
jneilliii <jneilliii+github@gmail.com>
2024-07-14 15:14:23 -0400

    0.1.6 * refactored processing logic

--- octoprint_prusafilamentrunoutmonitor/__init__.py
+++ octoprint_prusafilamentrunoutmonitor/__init__.py
@@ -41,39 +41,40 @@ class PrusafilamentrunoutmonitorPlugin(octoprint.plugin.SettingsPlugin,
 
     # ~~ GCode received hook
     def process_gcode(self, comm, line, *args, **kwargs):
-        if line.strip() == "echo:busy: processing" or self._processing:
+        if line.strip() == "echo:busy: processing" or self._processing and self._printer.is_printing():
             if line.strip() == "echo:busy: processing":
                 self._logger.debug("Enabling position monitor")
                 self._processing = True
-            # elif line.strip() == "ok" and self._printer.is_paused():
-            #     self._logger.debug("Filament change completed on printer.")
-            #     self._processing = False
-            #     if self._settings.get_boolean(["pause_print"]):
-            #         self._logger.debug("Resuming print job after filament change.")
-            #         self._printer.resume_print()
             elif self._processing:
                 x_position = self._settings.get(["x_position"])
                 y_position = self._settings.get(["y_position"])
-                if line.strip().startswith(f"X:{x_position} Y:{y_position}"):
+                if line.strip() == "ok":
+                    self._logger.debug("Filament change completed on printer.")
+                    self._processing = False
+                    self._plugin_manager.send_plugin_message(self._identifier, {'filamentrunout': False})
+                    if self._settings.get_boolean(["pause_print"]) and self._printer.is_paused() or self._printer.is_pausing():
+                        self._logger.debug("Resuming print job after filament change.")
+                        self._printer.resume_print()
+                elif line.strip().startswith(f"X:{x_position} Y:{y_position}"):
                     self._logger.debug("Parked position matched")
-                    fileposition = comm.getFilePosition() if comm else None
-                    progress = comm.getPrintProgress() if comm else None
-                    job_data = self._printer.get_current_job()
-
-                    payload = job_data.get("file")
-                    payload["owner"] = ""
-                    payload["user"] = job_data.get("user")
-                    payload["fileposition"] = fileposition
-                    payload["progress"] = progress
 
                     self._plugin_manager.send_plugin_message(self._identifier, {'filamentrunout': True})
                     self._processing = False
 
                     if self._settings.get_boolean(["pause_print"]):
-                        self._logger.debug("Faking acknowledgement and returning pause action.")
+                        self._logger.debug("Faking acknowledgement and returning paused action.")
                         self._printer.fake_ack()
                         return "// action:paused"
                     else:
+                        fileposition = comm.getFilePosition() if comm else None
+                        progress = comm.getPrintProgress() if comm else None
+                        job_data = self._printer.get_current_job()
+
+                        payload = job_data.get("file")
+                        payload["owner"] = ""
+                        payload["user"] = job_data.get("user")
+                        payload["fileposition"] = fileposition
+                        payload["progress"] = progress
                         self._logger.debug("Firing pause event in lieu of pausing.")
                         self._event_bus.fire(Events.PRINT_PAUSED, payload)
                 else:
